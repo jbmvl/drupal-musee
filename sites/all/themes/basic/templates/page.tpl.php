fed1049465cd90122df6aba9e438c213a3dd50e7
Dias Aurélien <aurelien.esiao@gmail.com>
2013-11-05 15:25:37 +0100

    header done need to JS search

--- sites/all/themes/basic/templates/page.tpl.php
+++ sites/all/themes/basic/templates/page.tpl.php
@@ -41,6 +41,7 @@
     <?php if ($main_menu || $secondary_menu): ?>
       <nav id="navigation" class="menu <?php if (!empty($main_menu)) {print "with-primary";}
         if (!empty($secondary_menu)) {print " with-secondary";} ?>">
+                <a href="#search" id="search_btn"><svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 32 32" enable-background="new 0 0 32 32" xml:space="preserve"><path d="M30.527,26.755l-5.901-5.546c1.619-2.208,2.586-4.919,2.586-7.862C27.213,5.987,21.226,0,13.865,0C6.505,0,0.518,5.987,0.518,13.347s5.987,13.347,13.347,13.347c2.212,0,4.295-0.548,6.134-1.505l6.373,5.987C26.958,31.729,27.703,32,28.448,32c0.808,0,1.616-0.321,2.211-0.956C31.807,29.824,31.748,27.903,30.527,26.755z M13.865,21.382c-4.43,0-8.035-3.605-8.035-8.035c0-4.431,3.605-8.036,8.035-8.036c4.431,0,8.037,3.605,8.036,8.036C21.9,17.776,18.295,21.382,13.865,21.382z"/></svg></a>
         <?php print theme('links', array('links' => $main_menu, 'attributes' => array('id' => 'primary', 'class' => array('links', 'clearfix', 'main-menu')))); ?>
         <?php print theme('links', array('links' => $secondary_menu, 'attributes' => array('id' => 'secondary', 'class' => array('links', 'clearfix', 'sub-menu')))); ?>
       </nav> <!-- /navigation -->
