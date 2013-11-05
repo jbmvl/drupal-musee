56306ab14ade3a448212b31a9e2ccf48643bc18a
Dias Aurélien <aurelien.esiao@gmail.com>
2013-11-05 14:22:07 +0100

    starting to style menu

--- sites/all/themes/basic/templates/page.tpl.php
+++ sites/all/themes/basic/templates/page.tpl.php
@@ -3,50 +3,54 @@
   <!-- ______________________ HEADER _______________________ -->
 
   <header id="header">
-
-    <?php if ($logo): ?>
-      <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
-        <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>"/>
-      </a>
-    <?php endif; ?>
-
-    <?php if ($site_name || $site_slogan): ?>
-      <div id="name-and-slogan">
-
-        <?php if ($site_name): ?>
-          <?php if ($title): ?>
-            <div id="site-name">
-              <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><?php print $site_name; ?></a>
-            </div>
-          <?php else: /* Use h1 when the content title is empty */ ?>
-            <h1 id="site-name">
-              <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><?php print $site_name; ?></a>
-            </h1>
+    <div class="container">
+      <?php if ($logo): ?>
+            <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
+              <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>"/>
+            </a>
           <?php endif; ?>
-        <?php endif; ?>
 
-        <?php if ($site_slogan): ?>
-          <div id="site-slogan"><?php print $site_slogan; ?></div>
-        <?php endif; ?>
+          <?php if ($site_name || $site_slogan): ?>
+            <div id="name-and-slogan">
 
-      </div>
+              <?php if ($site_name): ?>
+                <?php if ($title): ?>
+                  <div id="site-name">
+                    <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><?php print $site_name; ?></a>
+                  </div>
+                <?php else: /* Use h1 when the content title is empty */ ?>
+                  <h1 id="site-name">
+                    <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><?php print $site_name; ?></a>
+                  </h1>
+                <?php endif; ?>
+              <?php endif; ?>
+
+              <?php if ($site_slogan): ?>
+                <div id="site-slogan"><?php print $site_slogan; ?></div>
+              <?php endif; ?>
+
+            </div>
+          <?php endif; ?>
+
+          <?php if ($page['header']): ?>
+            <div id="header-region">
+              <?php print render($page['header']); ?>
+            </div>
+          <?php endif; ?>      
+
+    <?php if ($main_menu || $secondary_menu): ?>
+      <nav id="navigation" class="menu <?php if (!empty($main_menu)) {print "with-primary";}
+        if (!empty($secondary_menu)) {print " with-secondary";} ?>">
+        <?php print theme('links', array('links' => $main_menu, 'attributes' => array('id' => 'primary', 'class' => array('links', 'clearfix', 'main-menu')))); ?>
+        <?php print theme('links', array('links' => $secondary_menu, 'attributes' => array('id' => 'secondary', 'class' => array('links', 'clearfix', 'sub-menu')))); ?>
+      </nav> <!-- /navigation -->
     <?php endif; ?>
 
-    <?php if ($page['header']): ?>
-      <div id="header-region">
-        <?php print render($page['header']); ?>
-      </div>
-    <?php endif; ?>
+    </div>
 
   </header> <!-- /header -->
 
-  <?php if ($main_menu || $secondary_menu): ?>
-    <nav id="navigation" class="menu <?php if (!empty($main_menu)) {print "with-primary";}
-      if (!empty($secondary_menu)) {print " with-secondary";} ?>">
-      <?php print theme('links', array('links' => $main_menu, 'attributes' => array('id' => 'primary', 'class' => array('links', 'clearfix', 'main-menu')))); ?>
-      <?php print theme('links', array('links' => $secondary_menu, 'attributes' => array('id' => 'secondary', 'class' => array('links', 'clearfix', 'sub-menu')))); ?>
-    </nav> <!-- /navigation -->
-  <?php endif; ?>
+
 
   <!-- ______________________ MAIN _______________________ -->
 
