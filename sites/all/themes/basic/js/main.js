bc5a7dda9aacf5652c92031bf56d1abaa2a19ea7
Dias Aurélien <aurelien.esiao@gmail.com>
2013-11-05 23:47:59 +0100

    search anim

--- sites/all/themes/basic/js/main.js
+++ sites/all/themes/basic/js/main.js
@@ -1,3 +1,26 @@
 (function($) {
-	$("#block-search-form").hide();
+
+	$jq(document).ready(function()
+	{
+
+		$jq("#block-search-form").hide();
+		var header_h = $jq("header").height();
+		$jq("a#search_btn").on("click",
+		
+			function()
+			{
+
+			$jq('#block-search-form').show(
+			    function(){
+
+				$jq(this).animate({
+					opacity: 1,
+					top: header_h+25
+				}, 300);
+
+		    	});
+		});
+
+	});
+
 })(jQuery);
